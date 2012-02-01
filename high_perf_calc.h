dc9e53546f48628cea522e57af30ca937d7f9f04
Yotam Oron <yotamoron@yahoo.com>
2012-02-01 09:11:18 +0200

    Indentation

--- high_perf_calc.h
+++ high_perf_calc.h
@@ -19,37 +19,37 @@
 #define __HIGH_PERF_CALC_H__
 
 typedef enum {
-	/* Packet has been verified */
-	verification_result_ok = 0,
-
-	/* There’s a space issue somewhere */
-	verification_result_space = 1,
-
-	/* A number is smaller than 0 or bigger than 65535 */
-	verification_result_overflow = 2,
-
-	/* A packet’s suffix missing or corrupted */
-	verification_result_bad_suffix = 3,
-		
+    /* Packet has been verified */
+    verification_result_ok = 0,
+    
+    /* There’s a space issue somewhere */
+    verification_result_space = 1,
+    
+    /* A number is smaller than 0 or bigger than 65535 */
+    verification_result_overflow = 2,
+    
+    /* A packet’s suffix missing or corrupted */
+    verification_result_bad_suffix = 3,
+    
     /* ... Any more ? ... */
-
-	verification_result_max
+    
+    verification_result_max
 } verification_result_t;
 
 /* Any initiation to be done */
 typedef void *(*verification_plugin_init_t)(void);
 
 /* Called on each packet for verification */
-  typedef verification_result_t (*verification_plugin_verify_packet_t)(char *packet, void *priv_data);
+typedef verification_result_t (*verification_plugin_verify_packet_t)(char *packet, void *priv_data);
 
 /* Close and free whatever needed */
 typedef void (*verification_plugin_close_t)(void *priv_data);
 
 typedef struct verification_plugin_t {
-  char                                *plugin_name;
-  verification_plugin_init_t          init;
-  verification_plugin_verify_packet_t verify;
-  verification_plugin_close_t         close;
+    char                                *plugin_name;
+    verification_plugin_init_t          init;
+    verification_plugin_verify_packet_t verify;
+    verification_plugin_close_t         close;
 } verification_plugin_t;
 
 #endif /* __HIGH_PERF_CALC_H__ */
